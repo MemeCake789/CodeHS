3a8788615a10b2fb217cb7b1ae3e8b7ab348f0a0
MemeCake789 <132317002+MemeCake789@users.noreply.github.com>
2023-10-06 17:42:03 +0000

    Creates files

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+import os
+import subprocess
+
+# so the main code just makes a list with all the .py files and allows you to run the file
+
+# List all Python files in the current directory
+files = [f for f in os.listdir('.') if f.endswith('.py')]
+print("Python files in current directory:")
+for i, file in enumerate(files):
+    print(f"{i+1}. {file}")
+
+# Ask the user which file to run
+file_number = int(input("Enter the number of the file you want to run: ")) - 1
+file_to_run = files[file_number]
+
+# Run the selected file
+subprocess.call(['python', file_to_run])
