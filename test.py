f03277a1251f43659f7e40cf8f5348b9036773f7
MemeCake789 <132317002+MemeCake789@users.noreply.github.com>
2023-10-11 00:49:47 +0000

    added colorama library

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print('Hello World!')
+from colorama import  Back, Style
+
+print(Back.GREEN + 'The text with Green background')
+print(Style.DIM + 'The text is DIM now')
+print(Style.RESET_ALL + 'The text is RESET now')
