3d0400aa25e0cdbc8a0b77d19c3ee4fb754e4bd8
MemeCake789 <132317002+MemeCake789@users.noreply.github.com>
2023-10-17 01:01:28 +0000

    update

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template
+import tkinter as tk
+import tempfile
+import webbrowser
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-		output = "Hello, world!"  # Replace this with your desired output
-		return render_template('index.html', output=output)
+		# Create and display the Tkinter window
+		window = tk.Tk()
+		canvas = tk.Canvas(window, width=400, height=400)
+		canvas.pack()
+
+		rectangle = canvas.create_rectangle(50, 50, 200, 150, fill="red")
+		oval = canvas.create_oval(250, 50, 350, 150, fill="blue")
+		line = canvas.create_line(50, 250, 350, 250)
+
+		# Save the image as a temporary file
+		temp_file = tempfile.NamedTemporaryFile(suffix=".html", delete=False)
+		canvas.postscript(file=temp_file.name, colormode="color")
+
+		# Close the Tkinter window
+		window.destroy()
+
+		# Render the temporary HTML file
+		return render_template('index.html', image_path=temp_file.name)
 
 if __name__ == '__main__':
-		app.run()
+		app.run(debug=True)
